e0acd41a3179e87c4295205fcdc7deedd723f8ea
RaihaniSue <sue.raih@gmail.com>
2023-10-16 02:21:37 -0700

    printf functions

---
+++ printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _printf - mimics to printf function
+ * @format : identifier
+ *
+ * Return: the integer
+ */
+int _printf(const char *format, ...)
+{
+	params p[] = {
+		{"%c", print_char},
+		{"%s", print_string},
+		{"%%", print_percent},
+	};
+	va_list args;
+	int i = 0, l = 0;
+	int j;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+Here:
+	while (format[i] = '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (p[j].ident[0] == format[i] && p[j].ident[1] == format[i + 1])
+			{
+				l = l + p[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		i++;
+		l++;
+	}
+	va_end(args);
+	return (l);
+}
